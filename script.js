599adfda8fc2432518247808347fc1e5b00bbb7e
Wellyngton Nascimento <wellyngton.nsantos1910@gmail.com>
2023-05-30 17:49:17 -0300

    Primeiro Commit - Criando site portofolio

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function ativaLetra (elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((Letra, i)=>{
+        setTimeout(()=>{
+            elemento.innerHTML += Letra;
+        }, 75 * i)
+    })
+}
+
+const titulo = document.querySelector('.digitando');
+ativaLetra(titulo); 
