d58594d1ca41342063dde84b07d1bb396bd87ecf
Wellyngton Nascimento <wellyngton.nsantos1910@gmail.com>
2023-06-02 09:55:45 -0300

    Uma partipe sobre responsividade feitos e um dos JS feito

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
-function ativaLetra (elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((Letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += Letra;
-        }, 75 * i)
-    })
+function escrevendoLetra(){
+    function ativaLetra (elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((Letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += Letra;
+            }, 75 * i)
+        })
+    }
+    
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo); 
 }
 
-const titulo = document.querySelector('.digitando');
-ativaLetra(titulo); 
+const ativaMenu = document.querySelector('.fa-bars');
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+})
