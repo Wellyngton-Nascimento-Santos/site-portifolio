7558cb37d31e16122a68188370ee31a5dbb02f24
Wellyngton Nascimento <wellyngton.nsantos1910@gmail.com>
2023-07-01 11:37:21 -0300

    50% do menu para mobile feito

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ function escrevendoLetra(){
 }
 
 const ativaMenu = document.querySelector('.fa-bars');
+const navMenu =  document.querySelector('header .navegacao-primaria')
 
 ativaMenu.addEventListener('click', ()=>{
     ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
 })
