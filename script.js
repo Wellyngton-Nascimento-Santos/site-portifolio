51b4050e4bd6ec4311388a558503ab38e7266ab4
Wellyngton Nascimento <wellyngton.nsantos1910@gmail.com>
2023-07-17 11:02:23 -0300

    Meus Projetos quase finalizado

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ function menuMobol(){
     })
 }
 
+menuMobol();
+
 
 function sobreMim(){
 
@@ -82,3 +84,71 @@ function sobreMim(){
 }
 
 sobreMim();
+
+
+const listaALL = document.querySelectorAll('.projects_armazenamento ul li');
+const buttonGeral = document.querySelectorAll('.projects_models ul li');
+const buttonALL = document.querySelectorAll('.projects_models .all');
+
+function removeClick(index){
+    buttonGeral.forEach((item)=>{
+        item.classList.remove('ativo');
+    });
+    buttonGeral[index].classList.add('ativo');
+}
+
+buttonGeral.forEach((item, index)=>{
+    item.addEventListener('click', ()=>{
+        removeClick(index);
+    })
+})
+
+
+function showLista(lista, buttom = "all"){
+    lista.forEach((item)=>{
+        item.classList.remove('ativo');
+    })
+    if(buttom == 'design'){
+        lista[0].classList.add('ativo');
+        lista[1].classList.add('ativo');
+    }
+    if(buttom == 'graphic'){
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+    }if(buttom == 'website'){
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
+    }if(buttom == 'all'){
+        lista[0].classList.add('ativo');
+        lista[1].classList.add('ativo');
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
+    }
+}
+
+buttonGeral.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        let currentButton = e.target;
+        if(currentButton.classList.contains('all')){
+            showLista(listaALL);
+        }
+        if(currentButton.classList.contains('design')){
+            showLista(listaALL, "design");
+        }
+        if(currentButton.classList.contains('graphic')){
+            showLista(listaALL, "graphic");
+        }
+        if(currentButton.classList.contains('website')){
+            showLista(listaALL, "website");
+        }
+        if(currentButton.classList.contains('all')){
+            showLista(listaALL, "all");
+        }
+    })
+})
