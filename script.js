6e9bb39e500d11281a3b3aad527d1f7b2c0c6b5d
Wellyngton Nascimento <wellyngton.nsantos1910@gmail.com>
2023-07-27 10:43:39 -0300

    Algumas edições nas descrições

--- script.js
+++ script.js
@@ -152,3 +152,21 @@ buttonGeral.forEach((item)=>{
         }
     })
 });
+
+const btn = document.getElementById("btnTop")
+
+btn.addEventListener('click', function(){
+    window.scrollTo(0,0)
+});
+
+document.addEventListener('scroll',ocultar);
+
+function ocultar(){
+    if(window.scrollY > 10){
+        btn.style.display = "flex"
+    }else{
+        btn.style.display = "none"
+    }
+}
+
+ocultar();
